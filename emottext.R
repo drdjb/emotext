e3b7e1b183971f2d00131654aa7d998f7dd36d4c
David <4532559+drdjb@users.noreply.github.com>
2021-09-17 14:51:21 +0100

    Create emottext.R

---
+++ emottext.R
@@ -0,0 +1,17 @@
+library(syuzhet)
+library(lubridate)
+library(ggplot2)
+library(scales)
+library(reshape2)
+
+csv <- read.csv("E:/XXXXXXXX/emotesttext.csv", stringsAsFactors = FALSE)
+mySentiment <- get_nrc_sentiment(csv$text)
+csv <- cbind(csv, mySentiment)
+sentimentTotals <- data.frame(colSums(csv[,c(5:12)]))
+names(sentimentTotals) <- "count"
+sentimentTotals <- cbind("sentiment" = rownames(sentimentTotals), sentimentTotals)
+rownames(sentimentTotals) <- NULL
+ggplot(data = sentimentTotals, aes(x = sentiment, y = count)) +
+  geom_bar(aes(fill = sentiment), stat = "identity") +
+  xlab("Emotion") + ylab("Total Count") + ggtitle("Total Emotion Score in all comments") +
+  theme(text=element_text(size=8, family="mono"))
